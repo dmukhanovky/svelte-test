b17aa88b74afb5babee461e4b95a912d96af2928
dmukhanovsky <dmukhanovsky@list.ru>
2024-03-16 18:36:26 +0300

    add main page and nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,7 @@
     text-align: center;
     position: relative;
 }
-    info-container {
+    .info-container {
     height: 100%;
     display: flex;
     flex-direction: column;
@@ -46,7 +46,7 @@
     font-size: 4rem;
     margin: 0;
 }
-info h1 p {
+.info h1 p {
 position: relative;
 font: inherit;
 margin: 0;
@@ -56,18 +56,18 @@ width: fit-content;
 font-weight: 800;
 letter-spacing: -0.03em;
 }
-info h2 {
-max-width: 65 ch;
+.info h2 {
+max-width: 65ch;
 font-weight: 200;
 margin-top: 0.50rem;
 }
 .background-div {
 background-color: #ecebe8;
 width: 100%;
-height: 56 vh;
+height: 56vh;
 margin: -205px auto;
 }
-info info-text {
+.info .info-text {
 position: relative;
 }
 .scroll-button {
